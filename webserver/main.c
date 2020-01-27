7bb79e778f49e6809c127553fa75bfd9bfd5da57
Maxence Bourdin <maxence.bourdin2.etu@univ-lille.fr>
2020-01-27 16:01:07 +0100

    Création main.c socket.h socket.c et implémentation

--- webserver/main.c
+++ webserver/main.c
@@ -1,12 +1,34 @@
-# include <stdio.h>
-# include <string.h>
-int main (int argc, char ** argv)
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>  
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include "socket.h"
+
+int main (void)
 {
-/* Arnold Robbins in the LJ of February ’95 , describing RCS */
-	if (argc > 1 && strcmp ( argv[1], "-advice" ) == 0) {
-		printf ("Don’t Panic !\n");
-		return 42;
+	int socket_serveur;
+	socket_serveur = creer_serveur(8080);
+	if (socket_serveur == -1) {
+		printf("Erreur lors de la création du serveur");
+		return -1;
 	}
-	printf ("Need an advice ?\n");
+
+	int socket_client;
+
+	while(1){
+
+		socket_client = accept(socket_serveur, NULL, NULL);
+		
+		if(socket_client == -1){
+			perror("accept");
+			return -1;
+		}
+
+		const char * message_bienvenue = "Bonjour, bienvenue sur mon serveur\n Ce serveur a ete cree par les soins de Maxence et Kevin\n Ce n'est que le début mais il devrait vite y avoir des ameliorations\n Voici un passage d'Harry Potter en anglais\n Cela vous permettra de travailler votre anglais\n et aussi vous rappeler quelques souvenirs\n\" if you want to go back, I won’t blame you, \" he [Harry] said.\n\" You can take the Cloak, I won’t need it now. \"\n\" Don’t be stupid, \" said Ron.\n\" We’re coming, \" said Hermione.\n";
+		write(socket_client, message_bienvenue, strlen(message_bienvenue));
+	}
+		
 	return 0;
 }
---
+++ webserver/socket.c
@@ -0,0 +1,34 @@
+#include "socket.h"
+#include <sys/types.h>  
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdio.h>
+
+
+int creer_serveur(int port){
+
+	struct sockaddr_in saddr;
+	saddr.sin_port = htons(port);
+	saddr.sin_family = AF_INET ; /* Socket ipv4 */
+	saddr.sin_addr.s_addr = INADDR_ANY; /* écoute sur toutes les interfaces */
+
+	int socket_serveur ;
+	socket_serveur = socket(AF_INET, SOCK_STREAM, 0);
+	
+	if(socket_serveur == -1){ 
+		perror("socket_serveur");
+		return -1;
+	}
+
+	if(bind(socket_serveur, (struct sockaddr *) &saddr, sizeof(saddr)) == -1){
+		perror("bind socker_serveur");
+		return -1;
+	}
+
+	if(listen(socket_serveur, 10) == -1){
+		perror("Listen socket_serveur");
+		return -1;
+	}
+
+	return socket_serveur;
+}
