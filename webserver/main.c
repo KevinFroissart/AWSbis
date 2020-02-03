cb14a153ea3c70ed71b809390cb32adf7363d425
Maxence Bourdin <maxence.bourdin2.etu@univ-lille.fr>
2020-02-03 14:54:24 +0100

    Ignore Sigpipe

--- webserver/main.c
+++ webserver/main.c
@@ -17,6 +17,7 @@ void initialiser_signaux(void)
 
 int main (void)
 {
+	initialiser_signaux();
 	int socket_serveur;
 	socket_serveur = creer_serveur(8080);
 	if (socket_serveur == -1) {
@@ -24,27 +25,29 @@ int main (void)
 		return -1;
 	}
 
-	int socket_client;
+	while(1) {
 
-	socket_client = accept(socket_serveur, NULL, NULL);
+		int socket_client;
 
-	if(socket_client == -1){
-		perror("accept");
-		return -1;
-	}
+		socket_client = accept(socket_serveur, NULL, NULL);
 
-	const char * message_bienvenue = "Bonjour, bienvenue sur mon serveur\n Ce serveur a ete cree par les soins de Maxence et Kevin\n Ce n'est que le début mais il devrait vite y avoir des ameliorations\n Voici un passage d'Harry Potter en anglais\n Cela vous permettra de travailler votre anglais\n et aussi vous rappeler quelques souvenirs\n\" if you want to go back, I won’t blame you, \" he [Harry] said.\n\" You can take the Cloak, I won’t need it now. \"\n\" Don’t be stupid, \" said Ron.\n\" We’re coming, \" said Hermione.\n";
-	char buff[256];
+		if(socket_client == -1){
+			perror("accept");
+			return -1;
+		}
 
-	write(socket_client, message_bienvenue, strlen(message_bienvenue));
-	memset(buff, 0, sizeof(buff));
+		const char * message_bienvenue = "Bonjour, bienvenue sur mon serveur\n Ce serveur a ete cree par les soins de Maxence et Kevin\n Ce n'est que le début mais il devrait vite y avoir des ameliorations\n Voici un passage d'Harry Potter en anglais\n Cela vous permettra de travailler votre anglais\n et aussi vous rappeler quelques souvenirs\n\" if you want to go back, I won’t blame you, \" he [Harry] said.\n\" You can take the Cloak, I won’t need it now. \"\n\" Don’t be stupid, \" said Ron.\n\" We’re coming, \" said Hermione.\n";
+		char buff[256];
 
-	while(read(socket_client, buff, sizeof(buff))){
-		write(socket_client, buff, sizeof(buff));
+		write(socket_client, message_bienvenue, strlen(message_bienvenue));
 		memset(buff, 0, sizeof(buff));
-	}
 
-	initialiser_signaux();
+		while(read(socket_client, buff, sizeof(buff))){
+			write(socket_client, buff, sizeof(buff));
+			memset(buff, 0, sizeof(buff));
+		}
+
+	}
 
 	return 0;
 }
